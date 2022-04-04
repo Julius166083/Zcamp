d3e7579ef9f0f02e40dda0ca8f21b3b2e4662841
Julius. W <87927403+Julius166083@users.noreply.github.com>
2022-04-04 07:42:58 +0300

    Dag spark job

--- week_7_project/data_lake_ingestion/Airflow/Dags/dag_spark_task.py
+++ week_7_project/data_lake_ingestion/Airflow/Dags/dag_spark_task.py
@@ -1,1 +1,42 @@
+import airflow
+from datetime import datetime, timedelta
+from airflow.operators.bash_operator import BashOperator
+import os
 
+from datetime import datetime
+from datetime import datetime, timedelta
+
+import airflow
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.sensors.filesystem import FileSensor
+from airflow.operators.python import PythonOperator
+from airflow.operators.python_operator import PythonOperator
+
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+
+import csv
+import requests
+import json
+import csv
+import requests
+import json
+import zipfile
+import requests
+from zipfile import ZipFile
+import shutil
+
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2022, 4, 3),
+    'retries': 10,
+	  'retry_delay': timedelta(hours=1)
+}
+with airflow.DAG('4th_Dag_spark_job_task',
+                  default_args=default_args,
+                  schedule_interval='0 1 * * *') as dag:
+                  spark_job_task = BashOperator(
+                      task_id='spark_task',
+                      bash_command="/usr/local/bin/python3.7 /opt/airflow/dags/tasks/dutch_energy_sparkscript.py")
