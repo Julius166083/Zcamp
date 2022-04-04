0bf9cc89c8eee8290b15e4b88931565171ae394e
Julius. W <87927403+Julius166083@users.noreply.github.com>
2022-04-04 07:38:06 +0300

    dag upload to GCS

--- week_7_project/data_lake_ingestion/Airflow/Dags/dag_Dutchenergy_toGCS.py
+++ week_7_project/data_lake_ingestion/Airflow/Dags/dag_Dutchenergy_toGCS.py
@@ -102,8 +102,8 @@ def download_parquetize_upload_dag(
 
 ## COTEQ_ELECTRICITY_FILES
 
-COTEQ_ELECTRICITY_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/Electricity/coteq_electricity_{{ execution_date.strftime(\'%Y\') }}.csv'
-COTEQ_ELECTRICITY_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/Electricity/coteq_electricity_{{ execution_date.strftime(\'%Y\') }}.parquet'
+COTEQ_ELECTRICITY_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Electricity/coteq_electricity_{{ execution_date.strftime(\'%Y\') }}.csv'
+COTEQ_ELECTRICITY_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Electricity/coteq_electricity_{{ execution_date.strftime(\'%Y\') }}.parquet'
 #COTEQ_ELECTRICITY_GCS_PATH_TEMPLATE = "ELECTRICITY_COTEQ/coteq_electricity/{{ execution_date.strftime(\'%Y\') }}/coteq_electricity_{{ execution_date.strftime(\'%Y\') }}.parquet"
 COTEQ_ELECTRICITY_GCS_PATH_TEMPLATE = "ELECTRICITY_COTEQ/coteq_electricity_{{ execution_date.strftime(\'%Y\') }}.parquet"
 
@@ -128,8 +128,8 @@ download_parquetize_upload_dag(
 
 ## RENDO_ELECTRICITY_FILES
 
-RENDO_ELECTRICITY_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/Electricity/rendo_electricity_{{ execution_date.strftime(\'%Y\') }}.csv'
-RENDO_ELECTRICITY_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/Electricity/rendo_electricity_{{ execution_date.strftime(\'%Y\') }}.parquet'
+RENDO_ELECTRICITY_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Electricity/rendo_electricity_{{ execution_date.strftime(\'%Y\') }}.csv'
+RENDO_ELECTRICITY_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Electricity/rendo_electricity_{{ execution_date.strftime(\'%Y\') }}.parquet'
 RENDO_ELECTRICITY_GCS_PATH_TEMPLATE = "ELECTRICITY_RENDO/rendo_electricity_{{ execution_date.strftime(\'%Y\') }}.parquet"
 
 rendo_electricity_dag = DAG(
@@ -152,8 +152,8 @@ download_parquetize_upload_dag(
 
 ## STEDIN_ELECTRICITY_FILES
 
-STEDIN_ELECTRICITY_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/Electricity/stedin_electricity_{{ execution_date.strftime(\'%Y\') }}.csv'
-STEDIN_ELECTRICITY_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/Electricity/stedin_electricity_{{ execution_date.strftime(\'%Y\') }}.parquet'
+STEDIN_ELECTRICITY_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Electricity/stedin_electricity_{{ execution_date.strftime(\'%Y\') }}.csv'
+STEDIN_ELECTRICITY_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Electricity/stedin_electricity_{{ execution_date.strftime(\'%Y\') }}.parquet'
 STEDIN_ELECTRICITY_GCS_PATH_TEMPLATE = "ELECTRICITY_STEDIN/stedin_electricity_{{ execution_date.strftime(\'%Y\') }}.parquet"
 
 stedin_electricity_dag = DAG(
@@ -176,8 +176,8 @@ download_parquetize_upload_dag(
 
 ## WESTLAND-INFRA_ELECTRICITY_FILES
 
-#'WESTLAND-INFRA_ELECTRICITY_CSV_FILE_TEMPLATE' = AIRFLOW_HOME + '/Electricity/westland-infra_electricity_{{ execution_date.strftime(\'%Y\') }}.csv'
-#'WESTLAND-INFRA_ELECTRICITY_PARQUET_FILE_TEMPLATE' = AIRFLOW_HOME + '/Electricity/westland-infra_electricity_{{ execution_date.strftime(\'%Y\') }}.parquet'
+#'WESTLAND-INFRA_ELECTRICITY_CSV_FILE_TEMPLATE' = AIRFLOW_HOME + '/datasets/energy/Electricity/westland-infra_electricity_{{ execution_date.strftime(\'%Y\') }}.csv'
+#'WESTLAND-INFRA_ELECTRICITY_PARQUET_FILE_TEMPLATE' = AIRFLOW_HOME + '/datasets/energy/Electricity/westland-infra_electricity_{{ execution_date.strftime(\'%Y\') }}.parquet'
 #'WESTLAND-INFRA_ELECTRICITY_GCS_PATH_TEMPLATE' = "ELECTRICITY_WESTLAND-INFRA/westland-infra_electricity_{{ execution_date.strftime(\'%Y\') }}.parquet"
 #
 #westland-infra_electricity_dag = DAG(
@@ -199,8 +199,8 @@ download_parquetize_upload_dag(
 #)'
 ## ENDINET_ELECTRICITY_FILES
 
-ENDINET_ELECTRICITY_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/Electricity/endinet_electricity_{{ execution_date.strftime(\'0101%Y\') }}.csv'
-ENDINET_ELECTRICITY_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/Electricity/endinet_electricity_{{ execution_date.strftime(\'0101%Y\') }}.parquet'
+ENDINET_ELECTRICITY_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Electricity/endinet_electricity_{{ execution_date.strftime(\'0101%Y\') }}.csv'
+ENDINET_ELECTRICITY_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Electricity/endinet_electricity_{{ execution_date.strftime(\'0101%Y\') }}.parquet'
 ENDINET_ELECTRICITY_GCS_PATH_TEMPLATE = "ELECTRICITY_ENDINET/endinet_electricity_{{ execution_date.strftime(\'0101%Y\') }}.parquet"
 
 endinet_electricity_dag = DAG(
@@ -223,8 +223,8 @@ download_parquetize_upload_dag(
 
 ## ENDURIS_ELECTRICITY_FILES
 
-ENDURIS_ELECTRICITY_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/Electricity/enduriselectricity_{{ execution_date.strftime(\'0101%Y\') }}.csv'
-ENDURIS_ELECTRICITY_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/Electricity/enduriselectricity_{{ execution_date.strftime(\'0101%Y\') }}.parquet'
+ENDURIS_ELECTRICITY_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Electricity/enduriselectricity_{{ execution_date.strftime(\'0101%Y\') }}.csv'
+ENDURIS_ELECTRICITY_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Electricity/enduriselectricity_{{ execution_date.strftime(\'0101%Y\') }}.parquet'
 ENDURIS_ELECTRICITY_GCS_PATH_TEMPLATE = "ELECTRICITY_ENDURIS/enduriselectricity_{{ execution_date.strftime(\'0101%Y\') }}.parquet"
 
 enduris_electricity_dag = DAG(
@@ -247,8 +247,8 @@ download_parquetize_upload_dag(
 
 ## ENEXIS_ELECTRICITY_FILES
 
-ENEXIS_ELECTRICITY_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/Electricity/enexis_electricity_{{ execution_date.strftime(\'0101%Y\') }}.csv'
-ENEXIS_ELECTRICITY_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/Electricity/enexis_electricity_{{ execution_date.strftime(\'0101%Y\') }}.parquet'
+ENEXIS_ELECTRICITY_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Electricity/enexis_electricity_{{ execution_date.strftime(\'0101%Y\') }}.csv'
+ENEXIS_ELECTRICITY_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Electricity/enexis_electricity_{{ execution_date.strftime(\'0101%Y\') }}.parquet'
 ENEXIS_ELECTRICITY_GCS_PATH_TEMPLATE = "ELECTRICITY_ENEXIS/enexis_electricity_{{ execution_date.strftime(\'0101%Y\') }}.parquet"
 
 enexis_electricity_dag = DAG(
@@ -271,8 +271,8 @@ download_parquetize_upload_dag(
 
 ## LIANDER_ELECTRICITY_FILES
 
-LIANDER_ELECTRICITY_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/Electricity/liander_electricity_{{ execution_date.strftime(\'0101%Y\') }}.csv'
-LIANDER_ELECTRICITY_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/Electricity/liander_electricity_{{ execution_date.strftime(\'0101%Y\') }}.parquet'
+LIANDER_ELECTRICITY_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Electricity/liander_electricity_{{ execution_date.strftime(\'0101%Y\') }}.csv'
+LIANDER_ELECTRICITY_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Electricity/liander_electricity_{{ execution_date.strftime(\'0101%Y\') }}.parquet'
 LIANDER_ELECTRICITY_GCS_PATH_TEMPLATE = "ELECTRICITY_LIANDER/liander_electricity_{{ execution_date.strftime(\'0101%Y\') }}.parquet"
 
 liander_electricity_dag = DAG(
@@ -301,8 +301,8 @@ download_parquetize_upload_dag(
 
 ## COTEQ_GAS_ENERGY_FILES
 
-COTEQ_GAS_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/Gas/coteq_gas_{{ execution_date.strftime(\'%Y\') }}.csv'
-COTEQ_GAS_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/Gas/coteq_gas_{{ execution_date.strftime(\'%Y\') }}.parquet'
+COTEQ_GAS_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Gas/coteq_gas_{{ execution_date.strftime(\'%Y\') }}.csv'
+COTEQ_GAS_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Gas/coteq_gas_{{ execution_date.strftime(\'%Y\') }}.parquet'
 COTEQ_GAS_GCS_PATH_TEMPLATE = "GAS_COTEQ/coteq_gas_{{ execution_date.strftime(\'%Y\') }}.parquet"
 
 
@@ -327,8 +327,8 @@ download_parquetize_upload_dag(
 
 ## RENDO_GAS_ENERGY_FILES
 
-RENDO_GAS_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/Gas/rendo_gas_{{ execution_date.strftime(\'%Y\') }}.csv'
-RENDO_GAS_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/Gas/rendo_gas_{{ execution_date.strftime(\'%Y\') }}.parquet'
+RENDO_GAS_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Gas/rendo_gas_{{ execution_date.strftime(\'%Y\') }}.csv'
+RENDO_GAS_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Gas/rendo_gas_{{ execution_date.strftime(\'%Y\') }}.parquet'
 RENDO_GAS_GCS_PATH_TEMPLATE = "GAS_RENDO/rendo_gas_{{ execution_date.strftime(\'%Y\') }}.parquet"
 
 rendo_gas_dag = DAG(
@@ -351,8 +351,8 @@ download_parquetize_upload_dag(
 
 ## STEDIN_GAS_ENERGY_FILES
 
-STEDIN_GAS_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/Gas/stedin_gas_{{ execution_date.strftime(\'%Y\') }}.csv'
-STEDIN_GAS_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/Gas/stedin_gas_{{ execution_date.strftime(\'%Y\') }}.parquet'
+STEDIN_GAS_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Gas/stedin_gas_{{ execution_date.strftime(\'%Y\') }}.csv'
+STEDIN_GAS_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Gas/stedin_gas_{{ execution_date.strftime(\'%Y\') }}.parquet'
 STEDIN_GAS_GCS_PATH_TEMPLATE = "GAS_STEDIN/stedin_gas_{{ execution_date.strftime(\'%Y\') }}.parquet"
 
 stedin_gas_dag = DAG(
@@ -375,8 +375,8 @@ download_parquetize_upload_dag(
 
 ## WESTLAND-INFRA_GAS_ENERGY_FILES
 
-#WESTLAND-INFRA_GAS_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/Gas/westland-infra_gas_{{ execution_date.strftime(\'%Y\') }}.csv'
-#WESTLAND-INFRA_GAS_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/Gas/westland-infra_gas_{{ execution_date.strftime(\'%Y\') }}.parquet'
+#WESTLAND-INFRA_GAS_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Gas/westland-infra_gas_{{ execution_date.strftime(\'%Y\') }}.csv'
+#WESTLAND-INFRA_GAS_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Gas/westland-infra_gas_{{ execution_date.strftime(\'%Y\') }}.parquet'
 #WESTLAND-INFRA_GAS_GCS_PATH_TEMPLATE = "GAS_WESTLAND-INFRA/westland-infra_gas_{{ execution_date.strftime(\'%Y\') }}.parquet"
 #
 #westland-infra_gas_dag = DAG(
@@ -399,8 +399,8 @@ download_parquetize_upload_dag(
 
 ## ENDINET_GAS_ENERGY_FILES
 
-ENDINET_GAS_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/Gas/endinet_gas_{{ execution_date.strftime(\'0101%Y\') }}.csv'
-ENDINET_GAS_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/Gas/endinet_gas_{{ execution_date.strftime(\'0101%Y\') }}.parquet'
+ENDINET_GAS_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Gas/endinet_gas_{{ execution_date.strftime(\'0101%Y\') }}.csv'
+ENDINET_GAS_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Gas/endinet_gas_{{ execution_date.strftime(\'0101%Y\') }}.parquet'
 ENDINET_GAS_GCS_PATH_TEMPLATE = "GAS_ENDINET/endinet_gas_{{ execution_date.strftime(\'0101%Y\') }}.parquet"
 
 endinet_gas_dag = DAG(
@@ -423,8 +423,8 @@ download_parquetize_upload_dag(
 
 ## ENDURIS_GAS_ENERGY_FILES
 
-ENDURIS_GAS_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/Gas/endurisgas_{{ execution_date.strftime(\'0101%Y\') }}.csv'
-ENDURIS_GAS_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/Gas/endurisgas_{{ execution_date.strftime(\'0101%Y\') }}.parquet'
+ENDURIS_GAS_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Gas/endurisgas_{{ execution_date.strftime(\'0101%Y\') }}.csv'
+ENDURIS_GAS_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Gas/endurisgas_{{ execution_date.strftime(\'0101%Y\') }}.parquet'
 ENDURIS_GAS_GCS_PATH_TEMPLATE = "GAS_ENDURIS/endurisgas_{{ execution_date.strftime(\'0101%Y\') }}.parquet"
 
 enduris_gas_dag = DAG(
@@ -447,8 +447,8 @@ download_parquetize_upload_dag(
 
 ## ENEXIS_GAS_ENERGY_FILES
 
-ENEXIS_GAS_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/Gas/enexis_gas_{{ execution_date.strftime(\'0101%Y\') }}.csv'
-ENEXIS_GAS_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/Gas/enexis_gas_{{ execution_date.strftime(\'0101%Y\') }}.parquet'
+ENEXIS_GAS_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Gas/enexis_gas_{{ execution_date.strftime(\'0101%Y\') }}.csv'
+ENEXIS_GAS_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Gas/enexis_gas_{{ execution_date.strftime(\'0101%Y\') }}.parquet'
 ENEXIS_GAS_GCS_PATH_TEMPLATE = "GAS_ENEXIS/enexis_gas_{{ execution_date.strftime(\'0101%Y\') }}.parquet"
 
 enexis_gas_dag = DAG(
@@ -471,8 +471,8 @@ download_parquetize_upload_dag(
 
 ## LIANDER_GAS_ENERGY_FILES
 
-LIANDER_GAS_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/Gas/liander_gas_{{ execution_date.strftime(\'0101%Y\') }}.csv'
-LIANDER_GAS_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/Gas/liander_gas_{{ execution_date.strftime(\'0101%Y\') }}.parquet'
+LIANDER_GAS_CSV_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Gas/liander_gas_{{ execution_date.strftime(\'0101%Y\') }}.csv'
+LIANDER_GAS_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/datasets/energy/Gas/liander_gas_{{ execution_date.strftime(\'0101%Y\') }}.parquet'
 LIANDER_GAS_GCS_PATH_TEMPLATE = "GAS_LIANDER/liander_gas_{{ execution_date.strftime(\'0101%Y\') }}.parquet"
 
 liander_gas_dag = DAG(
